4b5282feb4b58194d8a264e4b8e8da544cda7149
Jeroen Vrijkorte <jeroen@jeroenvrijkorte.nl>
2024-08-27 09:47:29 +0200

    Added some code.

---
+++ App/DecisionTreeRootNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App;
+
+internal class DecisionTreeRootNode
+{
+    protected const string PLACEHOLDER = "##placeholder##";
+
+    public string Answer { get; set; } = PLACEHOLDER;
+
+    public List<DecisionTreeNode> Replies { get; } = [];
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace App
+﻿using System.Text.Json;
+
+namespace App
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length != 2)
+            {
+                Console.WriteLine($"Usage: {args[0]} <filename.json>");
+                return 2;
+            }
+            string fileName = args[1];
+            using Stream utf8Json = File.OpenRead(fileName);
+            DecisionTreeRootNode? tree = JsonSerializer
+                .Deserialize<DecisionTreeRootNode>(utf8Json);
+            if (tree == null)
+            {
+                Console.WriteLine("Empty decision tree.");
+                return 2;
+            }
+            return Walk(tree);
+        }
+
+        private static int Walk(DecisionTreeRootNode rootNode)
+        {
+            Console.WriteLine("How do you start?");
+            return HandleUserInput(rootNode);
+        }
+
+        private static int HandleUserInput(DecisionTreeRootNode node)
+        {
+            string? userInput = Console.ReadLine();
+            if (userInput == null)
+            {
+                Console.WriteLine("Exiting.");
+                return -1;
+            }
+            if (userInput == node.Answer)
+            {
+                return Walk(node.Replies);
+            }
+            else
+            {
+                Console.WriteLine($"Wrong! {node.Answer} was correct.");
+                return 1;
+            }
+        }
+
+        private static int Walk(List<DecisionTreeNode> replies)
+        {
+            if (replies.Count == 0)
+            {
+                Console.WriteLine("Correct.");
+                return 0;
+            }
+            DecisionTreeNode node = replies[Random.Shared.Next(replies.Count)];
+            Console.WriteLine(node.Reply + ". What now?");
+            return HandleUserInput(node);
         }
     }
 }
